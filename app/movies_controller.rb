bd7c2ff52ff5c78c71b2851bfefa3a105f63f64d
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-01 20:19:23 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new
-  movie.title = "This is a title."
-  movie.save
+  movie = Movie.create(title: "This is a title.")
+  # movie.title = "This is a title."
+  movie
 end
 
 def can_be_created_with_a_hash_of_attributes
